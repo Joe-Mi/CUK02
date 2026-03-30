4fb109ac6ec369d3b6aa255024d003d2edb9c56d
felixbilly <felixbillyonyango@gmail.com>
2026-03-30 16:21:35 +0300

    commit all changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,34 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        if (!User::where('email', 'test@example.com')->exists()) {
+            User::factory()->create([
+                'name' => 'Test User',
+                'email' => 'test@example.com',
+            ]);
+        }
+
+        User::updateOrCreate(
+            ['email' => 'admin@admin.com'],
+            [
+                'name' => 'Admin',
+                'surname' => 'System',
+                'username' => 'admin',
+                'password' => bcrypt('password'),
+                'type' => 'admin',
+            ]
+        );
+
+        User::updateOrCreate(
+            ['email' => 'care@cuk.ac.ke'],
+            [
+                'name' => 'Care',
+                'surname' => 'cuk',
+                'username' => 'cuk',
+                'password' => bcrypt('care@cuk=2060'),
+                'type' => 'admin',
+            ]
+        );
+
     }
 }
